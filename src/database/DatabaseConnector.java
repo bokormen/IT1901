425a8cba7bc5c8efc7398f4fbd56b5b7caa5671b
Øyvind <yvind@78.91.46.186>
2013-09-16 14:56:56 +0200

    La til filer nødvendige for databasetilkobling og spørring

---
+++ src/database/DatabaseConnector.java
@@ -0,0 +1,14 @@
+package database;
+
+import java.lang.ClassNotFoundException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.DriverManager;
+
+//import com.mysql.jdbc.PreparedStatement;
+
+public class DatabaseConnector {
+
+}
