557326c64384fbce21db68106c7ace2671c16f7e
Øyvind <yvind@78.91.21.38>
2013-09-23 12:47:29 +0200

    nå er det en funksjon for å koble til databasen som fungerer

--- src/database/DatabaseConnector.java
+++ src/database/DatabaseConnector.java
@@ -6,9 +6,34 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.DriverManager;
-
-//import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.PreparedStatement;
 
 public class DatabaseConnector {
-
+	public static Connection con;
+	
+	/**
+	 * @author Oeyvind Klungland Lund
+	 * Denne koden åpner en tilkobling til databasen vi bruker i gruppe 10, hoesten 2013i faget IT1901 ved NTNU
+	 */
+	public static void open() {
+		try {
+			Class.forName("com.mysql.jdbcDrive");
+			String url = "jdbc:mysql://mysql.stud.ntnu.no/oyvilund_sheep"; //adressen til databasen
+			String user = "oyvilund_it1901"; //brukernavnet for aa koble til databasen
+			String pw = "passord"; //passordet for aa koble til databasen
+			con = DriverManager.getConnection(url,user,pw);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error " + e.getMessage());
+		} catch (SQLException e) {
+			System.out.println("Error " + e.getMessage());
+		}
+	}
+	
+	public static void close() {
+		try {
+			if (con != null) con.close();
+		} catch (SQLException e) {
+			System.out.println("Error " + e.getMessage());
+		}
+	}
 }
