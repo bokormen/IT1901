e5a9255231f3c2aedf9987fd3497cf426daf5214
Morten Lillehagen <morten_lillehagen@hotmail.com>
2013-10-07 13:48:41 +0200

    mypoijmt

---
+++ src/div/MyPoint.java
@@ -0,0 +1,28 @@
+package div;
+
+public class MyPoint {
+	private double latitude;
+	private double longitude;
+
+	public MyPoint(double latitude, double longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setX(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public void setY(double longitude) {
+		this.longitude = longitude;
+	}
+
+}
