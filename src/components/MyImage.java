9f35d25611ecc2f05029e639fa6dd1d686713ccb
andreas <andreas@.ntnu.no>
2013-10-24 11:50:32 +0200

    jobbet litt i MyMap

---
+++ src/components/MyImage.java
@@ -0,0 +1,16 @@
+package components;
+
+import java.awt.Image;
+
+public class MyImage {
+	protected Image image;
+	protected int x;
+	protected int y;
+
+	public MyImage(Image image, int x, int y) {
+		this.image = image;
+		this.x = x;
+		this.y = y;
+	}
+
+}
